fd5c93380072e0a3fd70a9bd6c75ad4279f750f1
Vaibhav Singh - IT Partner (Studiodiseno) <v.vaibhavsingh@administrators-MacBook-Air.local>
2024-11-06 02:32:31 +0530

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import Container from './src/Components/Container/Container';
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProductDetailScreen from './src/Screens/ProductDetailScreen/ProductDetailScreen';
+import PaymentScreen from './src/Screens/PaymentScreen/PaymentScreen';
+import CartScreen from './src/Screens/CartScreen/CartScreen';
+import BottomtabNavigator from './src/Navigators/BottomtabNavigator';
 
 
 
 function App(): React.JSX.Element {
-
+  const Stack = createNativeStackNavigator();
   return (
-    <Container>
-      <View style={{height:40, width:40, backgroundColor:'red'}}></View>
-    </Container>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen  name="Home" component={BottomtabNavigator} />
+        <Stack.Screen  name="ProductDetailScreen" component={ProductDetailScreen} />
+        <Stack.Screen  name="PaymentScreen" component={PaymentScreen} />
+        <Stack.Screen  name="CartScreen" component={CartScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
