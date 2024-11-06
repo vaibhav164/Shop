481ad93c1232071c795abaf47b9ba2ef069dfadd
Vaibhav Singh - IT Partner (Studiodiseno) <v.vaibhavsingh@administrators-MacBook-Air.local>
2024-11-07 03:09:48 +0530

    Updaed HomeScreen with Product List

--- src/Screens/HomeScreen/HomeScreen.tsx
+++ src/Screens/HomeScreen/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -6,119 +6,154 @@ import {
   FlatList,
   StyleSheet,
   Dimensions,
+  Platform,
+  Pressable,
+  SafeAreaView,
   ScrollView,
 } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 import Container from '../../Components/Container/Container';
-import { itemList } from '../../Utils/Constant';
+import {itemList, ScreenHeight, ScreenWidth} from '../../Utils/Constant';
+import {Module} from '../../Utils/Constant';
+import SearchInput from '../../Components/TextInput/TextInput';
+import NeighbourHoodItems from '../../Components/NeighbourHoodSpecial/NeighbourHoodItems';
 const {width: viewportWidth} = Dimensions.get('window');
-
 const banners = [
   {id: '1', image: 'https://picsum.photos/200/300'},
   {id: '2', image: 'https://picsum.photos/200/300'},
   {id: '3', image: 'https://picsum.photos/200/300'},
 ];
 
-const products = [
-  {
-    id: '1',
-    name: 'Product 1',
-    price: '$10.00',
-    image: 'https://picsum.photos/200',
-    tags: ['Free Delivery', 'Selling Fast'],
-  },
-  {
-    id: '2',
-    name: 'Product 2',
-    price: '$15.00',
-    image: 'https://picsum.photos/200',
-    tags: ['New Arrival'],
-  },
-  {
-    id: '3',
-    name: 'Product 3',
-    price: '$20.00',
-    image: 'https://picsum.photos/200',
-    tags: ['Free Delivery'],
-  },
-];
+
 interface HomScreenProps {
   navigation?: any;
 }
 const HomeScreen = ({navigation}: HomScreenProps) => {
+  const [reduceModule, setReduceModule] = useState<Boolean>(false);
+  const [activeModule, sestActiveModule] = useState<String>('')
+
   const renderBannerItem = ({item}) => (
-    <View style={{marginHorizontal:'2%'}}>
-    <Image 
-    source={{uri: item.image}} 
-    style={styles.bannerImage} 
-    />
+    <View style={{marginHorizontal: '2%'}}>
+      <Image source={{uri: item.image}} style={styles.bannerImage} />
     </View>
   );
 
   const renderProductItem = ({item}) => (
-    <View style={styles.productCard}>
-      <Image
-       source={{uri: item.image}} 
-       style={styles.productImage} 
-       />
-      <Text style={styles.productName}>{item.name}</Text>
-      <Text style={styles.productPrice}>{item.price}</Text>
-      <View style={styles.tagsContainer}>
-          <Text style={styles.tag}>
-            {item.category}
-          </Text>
-      </View>
-    </View>
+    <Pressable onPress={()=>{navigation.navigate('ProductDetailScreen',{product:item})}} style={styles.productCard}>
+      <Image source={{uri: item.image}} style={styles.productImage} />
+      <Text style={styles.productName}>{item.title.slice(0,30)}</Text>
+    </Pressable>
   );
 
+  const renderModules = ({item}) => (
+    <Pressable onPress={()=>sestActiveModule(item.title)} style={[{ width: ScreenWidth*0.245}]}>
+      {!reduceModule ? (
+        <View style={{justifyContent:'space-between', alignItems:'center', borderWidth:1, borderColor:activeModule == item.title ?'#ff4f01':'#fff', marginHorizontal: '2%',borderRadius:5, backgroundColor:activeModule == item.title ? "#ff4f01": '#fff',paddingVertical:'10%'}}>
+          <Image
+            source={
+              item.title == 'Exclusive'
+                ? require('../../Assets/images/exclusive.jpeg')
+                : item.title == 'Food'
+                ? require('../../Assets/images/Food.png')
+                : item.title == 'Payment'
+                ? require('../../Assets/images/payment.png')
+                : require('../../Assets/images/stores.jpeg')
+            }
+            style={{
+              width: 50,
+              height: 80,
+              borderRadius: 5,
+              resizeMode: 'contain',
+            }}
+          />
+          <Text style={[
+              {fontSize: 16, fontWeight: 'bold', marginBottom:'0.5%'},
+              {color: activeModule == item.title ? '#fff' : 'red'},
+            ]}>{item.title}</Text>
+        </View>
+      ) : (
+        <View
+          style={{
+            backgroundColor: activeModule == item.title ? '#ff4f01': '#fff',
+            borderWidth: 1,
+            marginHorizontal: '2%',
+            alignItems: 'center',
+            paddingVertical: '8%',
+            borderColor:activeModule == item.title ? '#ff4f01': '#fff',
+            borderRadius:5
+          }}>
+          <Text
+            style={[
+              {fontSize: 16, fontWeight: 'bold',paddingBottom:'1%'},
+              {color: reduceModule ? activeModule == item.title ? '#fff' : 'red': 'red'},
+            ]}>
+            {item.title}
+          </Text>
+        </View>
+      )}
+    </Pressable>
+  );
+  const onScrollEvent = (event: any) => {
+    let scrollY = event.nativeEvent.contentOffset.y;
+    console.log('YYYY____', scrollY);
+    Platform.OS == 'ios' && scrollY > 65
+      ? setReduceModule(true)
+      : Platform.OS == 'android' && scrollY > 1
+      ? setReduceModule(true)
+      : setReduceModule(false);
+  };
   return (
     <Container>
-    <ScrollView>
-      <Carousel
-        loop
-        width={viewportWidth}
-        height={viewportWidth*0.3}
-        autoPlay={false}
-        data={banners}
-        scrollAnimationDuration={1000}
-        onSnapToItem={index => console.log('current index:', index)}
-        renderItem={renderBannerItem}
-        style={{alignItems:'center', justifyContent:'center'}}
-      />
-      <FlatList
-        data={itemList}
-        renderItem={renderProductItem}
-        keyExtractor={item => item.id}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.productList}
-        style={{backgroundColor:'red'}}
-      />
-      <FlatList
-        data={itemList}
-        renderItem={renderProductItem}
-        keyExtractor={item => item.id}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.productList}
-      />
-      <FlatList
-        data={itemList}
-        renderItem={renderProductItem}
-        keyExtractor={item => item.id}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.productList}
-      />
-      <FlatList
-        data={itemList}
-        renderItem={renderProductItem}
-        keyExtractor={item => item.id}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.productList}
-      />
-    </ScrollView>
+      <SafeAreaView style={{flex:1}}>
+        <FlatList
+          data={Module}
+          renderItem={renderModules}
+          keyExtractor={item => item.id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.productList}
+          style={{backgroundColor:'#fff', marginTop:Platform.OS == 'android' && '10%', height:!reduceModule ?ScreenHeight*0.21:ScreenHeight*0.08}}
+        />
+        <ScrollView onScroll={onScrollEvent} scrollEventThrottle={16}>
+          <Carousel
+            loop
+            width={viewportWidth}
+            height={viewportWidth * 0.3}
+            autoPlay={false}
+            data={banners}
+            scrollAnimationDuration={1000}
+            onSnapToItem={index => console.log('current index:', index)}
+            renderItem={renderBannerItem}
+            style={{alignItems: 'center', justifyContent: 'center'}}
+          />
+          <SearchInput  onFocus={()=>{navigation.navigate('Search')}}/>
+          <NeighbourHoodItems />
+          <FlatList
+            data={itemList}
+            renderItem={renderProductItem}
+            keyExtractor={item => item.id}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.productList}
+          />
+          <FlatList
+            data={itemList}
+            renderItem={renderProductItem}
+            keyExtractor={item => item.id}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.productList}
+          />
+          <FlatList
+            data={itemList}
+            renderItem={renderProductItem}
+            keyExtractor={item => item.id}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.productList}
+          />
+        </ScrollView>
+      </SafeAreaView>
     </Container>
   );
 };
@@ -128,29 +163,25 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 100,
     borderRadius: 5,
-     resizeMode: 'cover',
-     backgroundColor: 'pink',
-     margin:10,
-     marginHorizontal:70
+    resizeMode: 'cover',
+    margin: 10,
+    marginHorizontal: 70,
   },
   productCard: {
     backgroundColor: '#fff',
-    borderRadius: 10,
     padding: 10,
     marginRight: 10,
     width: 150,
     alignItems: 'center',
-    elevation: 2,
   },
   productImage: {
-    width: 100,
-    height: 100,
+    width: 80,
+    height: 80,
     borderRadius: 5,
     resizeMode: 'contain',
   },
   productName: {
-    fontSize: 16,
-    fontWeight: 'bold',
+    fontSize: 13,
     marginVertical: 5,
   },
   productPrice: {
