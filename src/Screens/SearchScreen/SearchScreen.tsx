d662922e22e54723205276a8ed4dd649c695013c
Vaibhav Singh - IT Partner (Studiodiseno) <v.vaibhavsingh@administrators-MacBook-Air.local>
2024-11-08 02:04:24 +0530

    Updated Search Screen

--- src/Screens/SearchScreen/SearchScreen.tsx
+++ src/Screens/SearchScreen/SearchScreen.tsx
@@ -1,9 +1,9 @@
-import { View, Text, FlatList, Pressable, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import { View, Text, FlatList, Pressable, Image, StyleSheet } from 'react-native'
 import SearchInput from '../../Components/SearchTextInput/SearchTextInput'
 import Container from '../../Components/Container/Container'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { itemList } from '../../Utils/Constant'
+import { itemList, ScreenHeight, ScreenWidth } from '../../Utils/Constant'
+
 
 export default function SearchScreen({navigation}) {
   const renderProductItem = ({item}) => (
@@ -17,7 +17,7 @@ export default function SearchScreen({navigation}) {
   );
   return (
     <Container>
-        <SafeAreaView style={{justifyContent:'center'}}>
+        <View>
         <SearchInput closeIcon={true} />
         <FlatList
             data={itemList}
@@ -26,33 +26,46 @@ export default function SearchScreen({navigation}) {
             showsHorizontalScrollIndicator={false}
             contentContainerStyle={styles.productList}
           />
-      </SafeAreaView>
+          </View>
       </Container>
   )
 }
 
+
 const styles = StyleSheet.create({
   productCard: {
     backgroundColor: '#fff',
-    padding: 10,
-    marginRight: 10,
-    width: '100%',
-    // alignItems: 'center',
-    flexDirection:'row'
+    padding: ScreenWidth * 0.03,
+    marginRight: ScreenWidth * 0.03,
+    flexDirection: 'row',
+    marginBottom: ScreenHeight * 0.02,
+    borderRadius: 8,
+    elevation: 2,
   },
   productImage: {
-    width: 80,
-    height: 80,
+    width: ScreenWidth * 0.2,
+    height: ScreenWidth * 0.2,
     borderRadius: 5,
     resizeMode: 'contain',
+    marginRight:'5%'
+  },
+  productInfo: {
+    marginLeft: ScreenWidth * 0.04,
+    justifyContent: 'center',
+    flex: 1,
   },
   productName: {
-    fontSize: 13,
-    // fontWeight: 'bold',
-    marginVertical: 5,
+    fontSize: ScreenWidth * 0.035,
+    fontWeight: 'bold',
+    marginVertical: ScreenHeight * 0.005,
+    color: '#333',
+    overflow: 'hidden',
   },
   productList: {
-    paddingVertical: 10,
-    paddingHorizontal: 5,
+    paddingVertical: ScreenHeight * 0.02, 
+    paddingHorizontal: ScreenWidth * 0.03,
+    flexDirection: 'row',
+    flexWrap: 'wrap', 
+    justifyContent: 'space-between', 
   },
 });
