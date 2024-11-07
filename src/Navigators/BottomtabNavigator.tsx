33975b9d552f331d7bf1c5346b4bc0f7168a8ce7
Vaibhav Singh - IT Partner (Studiodiseno) <v.vaibhavsingh@administrators-MacBook-Air.local>
2024-11-08 01:49:53 +0530

    Added User Profile Screen in Bottom Tab

--- src/Navigators/BottomtabNavigator.tsx
+++ src/Navigators/BottomtabNavigator.tsx
@@ -7,6 +7,7 @@ import CartScreen from '../Screens/CartScreen/CartScreen';
 import Icon from 'react-native-vector-icons/FontAwesome'; 
 import { Text, View } from 'react-native';
 import SearchScreen from '../Screens/SearchScreen/SearchScreen';
+import UserProfileScreen from '../Screens/User/UserDetailScreen';
 const Tab = createBottomTabNavigator();
 export default function BottomtabNavigator() {
   return (
@@ -58,7 +59,7 @@ export default function BottomtabNavigator() {
         <Tab.Screen options={{ headerShown: false }}  name="Home" component={HomeScreen} />
         <Tab.Screen options={{ headerShown: false }}  name="Cart" component={CartScreen} />
         <Tab.Screen options={{ headerShown: false }}  name="Search" component={SearchScreen} />
-        <Tab.Screen options={{ headerShown: false }}  name="Profile" component={PaymentScreen} />
+        <Tab.Screen options={{ headerShown: false }}  name="Profile" component={UserProfileScreen} />
       </Tab.Navigator>
   )
 }
